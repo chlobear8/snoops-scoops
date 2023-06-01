c4092f7b19ed8a37be22f78bbf8d0ca586814216
Chloe ONeil <chloesporri@yahoo.com>
2023-06-01 15:35:16 -0700

    add NewFlavorForm.js and boiler

---
+++ src/components/NewFlavorForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewFlavorForm(props) {
+  return (
+    <React.Fragment>
+      <h3>Form.</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewFlavorForm;
