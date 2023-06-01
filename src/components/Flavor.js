bad39d0cbb546bdacb82d63608f9507c5a2e5868
Chloe ONeil <chloesporri@yahoo.com>
2023-06-01 10:21:56 -0700

    add Flavor.js and boiler

---
+++ src/components/Flavor.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Flavor() {
+  return (
+    <React.Fragment>
+      <h3>Cookie Monster Dough</h3>
+      <h3>Blue cookie dough</h3>
+    </React.Fragment>
+  );
+}
+
+export default Flavor;
