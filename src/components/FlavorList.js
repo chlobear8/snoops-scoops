d6b8428c4f1aad28e2b8722630d93f37ae098701
Chloe ONeil <chloesporri@yahoo.com>
2023-06-01 10:32:39 -0700

    add proptypes to flavorlist

--- src/components/FlavorList.js
+++ src/components/FlavorList.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Flavor from "./Flavor";
+import PropTypes from "prop-types";
 
-function FlavorList() {
+function FlavorList(props) {
   return (
-    <Flavor />
+    <React.Fragment>
+      <hr/>
+      <Flavor
+        name={flavor.name}
+        flavor={flavor.flavor}
+        price={flavor.price}
+        allergies={flavor.allergies}
+        id={flavor.id}
+        key={flavor.id} />
+    </React.Fragment>
   );
 }
+
+FlavorList.propTypes = {
+  flavorList: PropTypes.array
+};
 
 export default FlavorList;
