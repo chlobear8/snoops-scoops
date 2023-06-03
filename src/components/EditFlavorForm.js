b5364b4c5c841297c6b8e3797b07c1de9133b2a5
Chloe ONeil <chloesporri@yahoo.com>
2023-06-03 14:29:03 -0700

    add handleEditFlavorOnSubmission to EditFlavorForm

--- src/components/EditFlavorForm.js
+++ src/components/EditFlavorForm.js
@@ -3,9 +3,20 @@ import ReusableForm from "./ReusableForm";
 import PropTypes from "prop-types";
 
 function EditFlavorForm(props) {
+  const { flavor } = props;
+
+  function handleEditingFlavorFormSubmission(event) {
+    event.preventDefault();
+    props.onEditFlavor({name: event.target.name.value,
+      flavor: event.target.flavor.value,
+      price: event.target.price.value,
+      allergies: event.target.allergies.value,
+      id: flavor.id})
+  }
   return (
     <React.Fragment>
       <ReusableForm
+        formSubmissionHandler={handleEditingFlavorFormSubmission}
         buttonText="Update Flavor" />
     </React.Fragment>
   );
