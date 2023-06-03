6fa0216b2fa34268f0c1ef5a213764bda6b47698
Chloe ONeil <chloesporri@yahoo.com>
2023-06-03 12:37:35 -0700

    add FlavorDetail.js and boiler

---
+++ src/components/FlavorDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function FlavorDetail(props) {
+  return (
+    <React.Fragment>
+      <h1>Flavor Details</h1>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default FlavorDetail;
