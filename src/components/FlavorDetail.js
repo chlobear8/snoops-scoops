7568bcfd703c90423186e3116c54902dd94e3ede
Chloe ONeil <chloesporri@yahoo.com>
2023-06-03 12:58:22 -0700

    add flavor as an object to FlavorDetails.js

--- src/components/FlavorDetail.js
+++ src/components/FlavorDetail.js
@@ -1,12 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function FlavorDetail(props) {
+  const { flavor } = props;
   return (
     <React.Fragment>
       <h1>Flavor Details</h1>
+      <h3>{flavor.name} - {flavor.price}</h3>
+        <h3>{flavor.flavor}</h3>
+        <p><em>{flavor.allergies}</em></p>
       <hr/>
     </React.Fragment>
   );
 }
+
+FlavorDetail.propTypes = {
+  flavor: PropTypes.object
+};
 
 export default FlavorDetail;
