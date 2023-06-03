102e9a49b5e971b844c9507a1046a336d4f0e0b8
Chloe ONeil <chloesporri@yahoo.com>
2023-06-03 13:10:45 -0700

    adjust NewFlavorForm.js

--- src/components/NewFlavorForm.js
+++ src/components/NewFlavorForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { v4 } from 'uuid';
+import ReusableForm from "./ReusableForm";
 
 function NewFlavorForm(props) {
 
@@ -17,24 +18,8 @@ function NewFlavorForm(props) {
   }
   return (
     <React.Fragment>
-      <form onSubmit = {handleNewFlavorFormSubmission}>
-        <input 
-          type='text'
-          name='name'
-          placeholder='Ice Cream Name' />
-        <input
-          type='text'
-          name='flavor'
-          placeholder='Ice Cream Flavor' />
-        <input
-          type='number'
-          name='price'
-          placeholder='Pint Price' />
-        <input
-          type='text'
-          name='allergies'
-          placeholder='Allergens' />
-      </form>
+      <ReusableForm formSubmissionHandler={handleNewFlavorFormSubmission}
+      buttonText="New Flavor" />
     </React.Fragment>
   );
 }
